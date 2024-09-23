ba057ff878794fa3f5157c5e99a4aa9fde99a0b1
manigoa9652 <146149390+manigoa9652@users.noreply.github.com>
2024-09-22 22:36:20 -0400

    Add files via upload

---
+++ P1LAB1_ManigoAngel.py
@@ -0,0 +1,9 @@
+#Angel Manigo
+#9/22/24
+#P1LAB1
+#Create program for users to enter first and last name
+
+print('Enter your first name:', end=' ')
+first_name = input()
+print('Enter your last name:', end=' ')
+last_name = input()
